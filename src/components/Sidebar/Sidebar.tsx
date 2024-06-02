5dd5e9ca719eaa51402f3a3f5e94669012c8d89e
Nicklas Jensen <nicklas@nila.io>
2024-06-02 03:20:32 +0200

    style and implement sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,38 @@
+import { Link } from "react-router-dom";
 import styles from "./Sidebar.module.css";
+import logo from "/qonto.svg";
+
+const navigation = [
+  {
+    title: "History",
+    path: "/transations",
+  },
+  {
+    title: "Cards",
+    path: "/cards",
+  },
+];
 
 export const Sidebar = () => {
-  return <nav className={styles.navigation}></nav>;
+  return (
+    <nav className={styles.navigation}>
+      <Link to="/" className={styles.logo}>
+        <img src={logo} alt="Qonto logo" />
+      </Link>
+
+      <ul className={styles.navigationMenu}>
+        {navigation.map((navItem) => {
+          return (
+            <Link
+              key={navItem.title}
+              className={styles.navItem}
+              to={navItem.path}
+            >
+              {navItem.title}
+            </Link>
+          );
+        })}
+      </ul>
+    </nav>
+  );
 };
